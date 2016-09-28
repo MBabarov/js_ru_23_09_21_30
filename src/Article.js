be269b7f7ab481af9ae519de61ad90b15c3cbd69
Maxim Babarov <Maxim.Babarov@varian.com>
2016-09-28 16:32:20 +0300

    Homework-1: Add button to article

--- src/Article.js
+++ src/Article.js
@@ -1,51 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Article extends Component {
 
-    constructor(props) {
-        super()
-        this.state = {
-            isOpen: false,
-            foo: 'bar'
-        }
-    }
 
-/*
+
     state = {
         isOpen: false,
         foo: 'bar'
-    }
-*/
-
-    render() {
-        const { article } = this.props
-        const { isOpen } = this.state
-        console.log('---', this.state)
-
-        const body = isOpen ? <section>{article.text}</section> : null
-        // <section style = {{display: isOpen ? 'block' : 'none'}}>{article.text}</section>
-        return (
-            <div>
-                <h3 onClick = {this.toggleOpen}>{article.title}</h3>
-                {body}
-            </div>
-        )
-    }
+    };
 
     toggleOpen = ev => {
         this.setState({
             isOpen: !this.state.isOpen
         })
-    }
-}
+    };
 
-/*
-export default (props) => {
-    const { article } = props
-    return (
-        <div>
-            <h3>{article.title}</h3>
-            <section>{article.text}</section>
-        </div>
-    )
-}*/
+    render() {
+        const { article } = this.props;
+        const { isOpen } = this.state;
+
+        return (
+            <div>
+                <h3>{article.title}</h3>
+                <section>
+                    <div style = {{display: isOpen ? 'block' : 'none'}}>{article.text}<br /><br /></div>
+                    <button onClick = {this.toggleOpen}>{isOpen ? 'Hide' : 'Show more'}</button>
+                </section>
+            </div>
+        )
+    }
+
+}
