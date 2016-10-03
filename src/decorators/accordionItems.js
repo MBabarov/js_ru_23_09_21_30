6fda1ee7184c909fafb58258cb866482962584b1
Roman <kaboom.ua@gmail.com>
2016-10-03 22:23:10 +0300

    Update accordionItems.js

--- src/decorators/accordionItems.js
+++ src/decorators/accordionItems.js
@@ -4,9 +4,11 @@ export default function accordionItems(Component) {
     return class WrapperComponent extends React.Component{
 
         state = {
+            //Не привязывайся к названию сущности, декоратор будет использоваться везде. Назови, скажем, openItemId
             openArticleId: null
         };
         openArticle = id => ev => {
+            //Но проще this.setState({openArticleId: this.state.openArticleId==id ? null : id})
             (this.state.openArticleId==id) ? this.setState({openArticleId: null}) : this.setState({openArticleId: id})
         };
         isOpen  = id  => this.state.openArticleId === id;
